3b355aa1646006888e2a4d4ae0b1f7d1c8abcd03
Elliott Chartock <elboy@stanford.edu>
2018-06-09 18:51:47 -0700

    split data

--- pytorch_model.py
+++ pytorch_model.py
@@ -37,7 +37,7 @@ class Net(nn.Module):
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        #x = F.dropout(x, training=self.training)
+        x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return x
 
